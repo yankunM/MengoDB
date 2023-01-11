4b424f9c3a611c0bd4090b0040d4cb4c64ef32b2
yankunM <91627484+yankunM@users.noreply.github.com>
2023-01-11 01:12:10 -0500

    Add files via upload

---
+++ src/VisitRecord.java
@@ -0,0 +1,27 @@
+import java.time.LocalDateTime;
+
+public class VisitRecord {
+    // Attributes
+    String name;
+    LocalDateTime entry;
+    LocalDateTime exit;
+
+    // Constructors
+    public VisitRecord(String n, LocalDateTime en, LocalDateTime ex) throws Exception {
+        if(ex.isBefore(en)) throw new InvalidEntryExitTimeException();
+        this.name = n;
+        this.entry = en;
+        this.exit = ex;
+    }
+    public VisitRecord(String n, LocalDateTime en) throws Exception {
+        this.name = n;
+        this.entry = en;
+        this.exit = null;
+    }
+}
+
+class InvalidEntryExitTimeException extends Exception {
+    public InvalidEntryExitTimeException(){
+        super();
+    }
+}
